c2c0ed7524201716495188f7bf0771269e9d0b3b
tyVespA <migliavacca.marco08@gmail.com>
2023-02-20 22:34:44 +0100

    Complete button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Button from "./components/Button";
 
+import LocalGroceryStoreOutlinedIcon from "@mui/icons-material/LocalGroceryStoreOutlined";
+
 function App() {
   return (
     <div className="App">
@@ -52,6 +54,51 @@ function App() {
         <div className="variantContainer">
           <p>{"&:hover, &:focus"}</p>
           <Button disabled hover />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="variantContainerWide">
+          <p>{"<Button startIcon=”local_grocery_store” />"}</p>
+          <Button startIcon="local_grocery_store" />
+        </div>
+        <div className="variantContainerWide">
+          <p>{"<Button startIcon=”local_grocery_store” />"}</p>
+          <Button endIcon="local_grocery_store" />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="variantContainer">
+          <p>{"<Button size=”sm” />"}</p>
+          <Button size="sm" />
+        </div>
+        <div className="variantContainer">
+          <p>{"<Button size=”md” />"}</p>
+          <Button size="md" />
+        </div>
+        <div className="variantContainer">
+          <p>{"<Button size=”lg” />"}</p>
+          <Button size="lg" />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="variantContainerWide">
+          <p>{"<Button color=”default” />"}</p>
+          <Button color="default" />
+        </div>
+        <div className="variantContainerWide">
+          <p>{"<Button color=”default” />"}</p>
+          <Button color="primary" />
+        </div>
+        <div className="variantContainerWide">
+          <p>{"<Button color=”default” />"}</p>
+          <Button color="secondary" children="Secondary" />
+        </div>
+        <div className="variantContainerWide">
+          <p>{"<Button color=”default” />"}</p>
+          <Button color="danger" children="Danger" />
         </div>
       </div>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import s from "./Button.module.css";
+import LocalGroceryStoreOutlinedIcon from "@mui/icons-material/LocalGroceryStoreOutlined";
 
-function Button({ variant, hover, disableShadow, disabled }) {
+function Button({
+  children,
+  variant,
+  hover,
+  disableShadow,
+  disabled,
+  startIcon,
+  endIcon,
+  size,
+  color,
+}) {
   return (
     <button
       className={`${variant ? s[variant] : ""} ${hover ? s.hover : ""} ${
         disableShadow ? s.disableShadow : ""
-      } ${disabled ? s.disabled : ""}`}
+      } ${disabled ? s.disabled : ""} ${startIcon ? s.startIcon : ""} ${
+        endIcon ? s.endIcon : ""
+      } ${size ? s[size] : ""} ${color ? s[color] : ""}`}
     >
-      Default
+      {(startIcon && <LocalGroceryStoreOutlinedIcon fontSize="small" />) ||
+        (endIcon && <LocalGroceryStoreOutlinedIcon fontSize="small" />)}{" "}
+      {children ? children : "Default"}
     </button>
   );
 }
